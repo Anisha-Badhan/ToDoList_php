d26366599261a5c9bc7794d5ad04fd3f6618dce5
Anisha Badhan <anishasd1983@gmail.com>
2021-02-24 19:34:54 -0500

    validation

--- index.php
+++ index.php
@@ -8,9 +8,19 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script>
+        function validateForm() {
+            var x = document.forms["myForm"]["task"].value;
+            if (x == "") {
+                alert("Task must be filled out");
+                return false;
+            }
+        }
+    </script>
 </head>
 <body>
 <div class="container">
+
     <?php
     if(isset($_GET['taskAdded'])){
         ?>
@@ -30,7 +40,7 @@
     }
     ?>
     <h2>My List!</h2>
-    <form action="lib/toDoList.php" method="get">
+    <form name="myForm" action="lib/toDoList.php" onsubmit="return validateForm()" method="get">
         <div class="form-group">
             <label for="task">Task:</label>
             <input type="text" class="form-control" id="task" name="task" >
